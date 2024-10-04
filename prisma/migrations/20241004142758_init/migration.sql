5869a6d6be14fa805a9f867454e477059085d8e1
pcr9x <petchppw@gmail.com>
2024-10-04 23:26:18 +0700

    feat: add db and auth setup

---
+++ prisma/migrations/20241004142758_init/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "species" TEXT NOT NULL,
+    "breed" TEXT,
+    "sex" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "reward" DOUBLE PRECISION,
+    "photo" TEXT NOT NULL,
+    "locationId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Location" (
+    "id" SERIAL NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "address" TEXT,
+
+    CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/lib/auth.ts
@@ -0,0 +1,54 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { db } from "./db";
+import { compare } from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "john-doe@gmail.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+					return null;
+				}
+
+				const existingUser = await db.user.findUnique({
+					where: { email: credentials.email },
+				});
+
+				if (!existingUser) {
+					return null;
+				}
+
+				const isPasswordValid = await compare(
+					credentials.password,
+					existingUser.password
+				);
+
+				if (!isPasswordValid) {
+					return null;
+				}
+
+				return {
+					id: `${existingUser.id}`,
+					username: existingUser.username,
+					email: existingUser.email,
+				};
+
+      },
+    }),
+  ],
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/flow/login",
+  }
+};
