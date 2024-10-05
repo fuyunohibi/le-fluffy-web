b6e96501d3c05faa230540a8e644405b3ed0daf7
SSYino <gianmario.nino@gmail.com>
2024-10-06 00:14:45 +0700

    Add delete post by id route

--- src/app/(core)/(backend)/api/posts/[id]/route.ts
+++ src/app/(core)/(backend)/api/posts/[id]/route.ts
@@ -18,3 +18,31 @@ export async function GET(_: Request, context: { params: { id: string } }) {
         return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
     }
 }
+
+export async function DELETE(_: Request, context: { params: { id: string } }) {
+    try {
+        const postId = +context.params.id;
+
+        // Check if the post exists before attempting to delete
+        const post = await db.post.findUnique({
+            where: { id: postId }
+        });
+
+        if (!post) {
+            return NextResponse.json({ message: "Post not found" }, { status: 404 });
+        }
+
+        // Delete the post
+        await db.post.delete({
+            where: { id: postId }
+        });
+
+        return NextResponse.json(
+            { message: `Post for ${post.name} deleted successfully` },
+            { status: 200 }
+        );
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
+    }
+}
