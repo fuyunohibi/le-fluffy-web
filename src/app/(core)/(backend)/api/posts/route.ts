b06ca6905eda9a8b298643fbeb426e094580d36a
SSYino <gianmario.nino@gmail.com>
2024-10-06 00:03:53 +0700

    Init create post API

---
+++ src/app/(core)/(backend)/api/posts/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    try {
+        const {
+            name, species, breed, sex, description, reward, photo, locationId, userId
+        } = await req.json();
+
+        const newPost = await db.post.create({
+            data: { name, species, breed, sex, description, reward, photo, locationId, userId }
+        });
+
+        return NextResponse.json(
+            { message: `Post for ${newPost.name} created successfully` },
+            { status: 201 }
+        );
+    } catch (err) {
+        console.log(err)
+        return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
+    }
+}
