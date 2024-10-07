447a449d00a1023f250138de778a653aee4b95d2
pcr9x <petchppw@gmail.com>
2024-10-07 16:00:13 +0700

    use getSession in post

--- src/app/(core)/(backend)/api/posts/route.ts
+++ src/app/(core)/(backend)/api/posts/route.ts
@@ -1,22 +1,43 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export async function POST(req: Request) {
-    try {
-        const {
-            name, species, breed, sex, description, reward, photo, userId
-        } = await req.json();
-
-        const newPost = await db.post.create({
-            data: { name, species, breed, sex, description, reward, photo, userId }
-        });
-
-        return NextResponse.json(
-            { message: `Post for ${newPost.name} created successfully` },
-            { status: 201 }
-        );
-    } catch (err) {
-        console.log(err)
-        return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
+
+    const userId = parseInt(session.user.id);
+
+    const { name, species, breed, sex, description, reward, photo } =
+      await req.json();
+
+    // Create the post without the location field
+    const newPost = await db.post.create({
+      data: {
+        name,
+        species,
+        breed,
+        sex,
+        description,
+        reward,
+        photo,
+        userId,
+      },
+    });
+
+    return NextResponse.json(
+      { message: `Post for ${newPost.name} created successfully` },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { message: "Something went wrong!" },
+      { status: 500 }
+    );
+  }
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -49,7 +49,7 @@ export const authOptions: NextAuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
-    signIn: "/flow/login",
+    signIn: "/auth",
   },
   callbacks: {
     async jwt({ token, user }) {
