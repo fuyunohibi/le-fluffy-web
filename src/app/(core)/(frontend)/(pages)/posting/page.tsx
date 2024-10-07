cc6ef7517496f6332ba05a8ef129080e7d8bafc8
Kokwan Mongkholtham <kokwan.m@gmail.com>
2024-10-08 01:36:25 +0700

    feat: fix posting page

--- src/app/(core)/(frontend)/(pages)/posting/page.tsx
+++ src/app/(core)/(frontend)/(pages)/posting/page.tsx
@@ -105,160 +105,168 @@ const PostingPage: React.FC = () => {
 
   return (
     <section className="min-h-screen flex justify-center items-center ">
-      <BackgroundGradient className="bg-white p-10 rounded-3xl shadow-lg w-[45rem] mx-auto">
-          <div className="mb-6 text-center">
-            <h1 className="text-4xl font-extrabold text-gray-800">
-              Post your missing pet 🐈
-            </h1>
-            <p className="text-gray-600 mt-3">
-              Help us find your missing pet by posting their details here.
-            </p>
+      <BackgroundGradient className="bg-white p-10 rounded-3xl shadow-lg w-[60rem] mx-auto">
+        <div className="mb-6 text-center">
+          <h1 className="text-4xl font-extrabold text-gray-800">
+            Post your missing pet 🐈
+          </h1>
+          <p className="text-gray-600 mt-3">
+            Help us find your missing pet by posting their details here.
+          </p>
+        </div>
+        <form
+          className="space-y-6 flex flex-col justify-center"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex justify-between items-start space-x-10">
+            <div className="w-full">
+              <div className="relative mb-4">
+                <label
+                  htmlFor="image"
+                  className="block text-sm font-bold text-gray-700 mb-2"
+                >
+                  Image URL
+                </label>
+                <input
+                  type="url"
+                  name="image"
+                  id="image"
+                  value={formData.image}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
+                  placeholder="https://example.com/image.jpg"
+                  required
+                />
+              </div>
+
+              <div className="relative mb-4">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-bold text-gray-700 mb-2"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
+                  placeholder="Milo"
+                  required
+                />
+              </div>
+
+              <div className="relative mb-4">
+                <label
+                  htmlFor="description"
+                  className="block text-sm font-bold text-gray-700 mb-2"
+                >
+                  Description
+                </label>
+                <textarea
+                  name="description"
+                  id="description"
+                  value={formData.description}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 rounded-2xl shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
+                  rows={4}
+                  placeholder="A happy-go-lucky Labrador Retriever with a love for water."
+                  required
+                />
+              </div>
+
+              <div className="relative mb-4">
+                <label
+                  htmlFor="sex"
+                  className="block text-sm font-bold text-gray-700 mb-2"
+                >
+                  Sex
+                </label>
+                <select
+                  name="sex"
+                  id="sex"
+                  value={formData.sex}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
+                  required
+                >
+                  <option value="">Select sex</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="w-full">
+              <div className="relative mb-4">
+                <label
+                  htmlFor="age"
+                  className="block text-sm font-bold text-gray-700 mb-2"
+                >
+                  Age
+                </label>
+                <input
+                  type="number"
+                  step="any"
+                  name="age"
+                  id="age"
+                  value={formData.age}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
+                  placeholder="2.5"
+                  required
+                />
+              </div>
+
+              <div className="relative mb-4">
+                <label
+                  htmlFor="species"
+                  className="block text-sm font-bold text-gray-700 mb-2"
+                >
+                  Species
+                </label>
+                <input
+                  type="text"
+                  name="species"
+                  id="species"
+                  value={formData.species}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
+                  placeholder="Dog"
+                  required
+                />
+              </div>
+
+              <div className="relative mb-4">
+                <label
+                  htmlFor="reward"
+                  className="block text-sm font-bold text-gray-700 mb-2"
+                >
+                  Reward
+                </label>
+                <input
+                  type="number"
+                  step="any"
+                  name="reward"
+                  id="reward"
+                  min="0"
+                  value={formData.reward}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
+                  placeholder="5000 bath"
+                />
+              </div>
+            </div>
           </div>
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div className="relative mb-4">
-              <label
-                htmlFor="image"
-                className="block text-sm font-bold text-gray-700 mb-1"
-              >
-                Image URL
-              </label>
-              <input
-                type="url"
-                name="image"
-                id="image"
-                value={formData.image}
-                onChange={handleChange}
-                className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
-                placeholder="https://example.com/image.jpg"
-                required
-              />
-            </div>
-
-            <div className="relative mb-4">
-              <label
-                htmlFor="name"
-                className="block text-sm font-bold text-gray-700 mb-1"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                value={formData.name}
-                onChange={handleChange}
-                className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
-                placeholder="Milo"
-                required
-              />
-            </div>
-
-            <div className="relative mb-4">
-              <label
-                htmlFor="description"
-                className="block text-sm font-bold text-gray-700 mb-1"
-              >
-                Description
-              </label>
-              <textarea
-                name="description"
-                id="description"
-                value={formData.description}
-                onChange={handleChange}
-                className="w-full px-4 py-2 rounded-2xl shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
-                rows={4}
-                placeholder="A happy-go-lucky Labrador Retriever with a love for water."
-                required
-              />
-            </div>
-
-            <div className="relative mb-4">
-              <label
-                htmlFor="sex"
-                className="block text-sm font-bold text-gray-700 mb-1"
-              >
-                Sex
-              </label>
-              <select
-                name="sex"
-                id="sex"
-                value={formData.sex}
-                onChange={handleChange}
-                className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
-                required
-              >
-                <option value="">Select sex</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </select>
-            </div>
-
-            <div className="relative mb-4">
-              <label
-                htmlFor="age"
-                className="block text-sm font-bold text-gray-700 mb-1"
-              >
-                Age
-              </label>
-              <input
-                type="number"
-                step="any"
-                name="age"
-                id="age"
-                value={formData.age}
-                onChange={handleChange}
-                className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
-                placeholder="2.5"
-                required
-              />
-            </div>
-
-            <div className="relative mb-4">
-              <label
-                htmlFor="species"
-                className="block text-sm font-bold text-gray-700 mb-1"
-              >
-                Species
-              </label>
-              <input
-                type="text"
-                name="species"
-                id="species"
-                value={formData.species}
-                onChange={handleChange}
-                className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
-                placeholder="Dog"
-                required
-              />
-            </div>
-
-            <div className="relative mb-4">
-              <label
-                htmlFor="reward"
-                className="block text-sm font-bold text-gray-700 mb-1"
-              >
-                Reward
-              </label>
-              <input
-                type="number"
-                step="any"
-                name="reward"
-                id="reward"
-                min="0"
-                value={formData.reward}
-                onChange={handleChange}
-                className="w-full px-4 py-2 rounded-full shadow-md focus:border-blue-500 focus:ring focus:ring-blue-200 bg-gray-100"
-                placeholder="5000 bath"
-              />
-            </div>
-
-            <button
-              type="submit"
-              className="w-full py-3 rounded-full bg-blue-500 hover:bg-blue-600 text-white font-bold shadow-lg transition-all duration-300 transform hover:scale-105"
-            >
-              Post
-            </button>
-          </form>
+          <button
+            type="submit"
+            className="w-full py-3 rounded-full bg-blue-500 hover:bg-blue-600 text-white font-bold shadow-lg transition-all duration-300 transform hover:scale-105"
+          >
+            Post
+          </button>
+        </form>
       </BackgroundGradient>
       <Notification visible={notification} icon={<CheckIcon />} />
       <FailedNotification visible={failedNotification} icon={<XMarkIcon />} />
